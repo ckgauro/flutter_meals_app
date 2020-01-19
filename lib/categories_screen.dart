7764ffb2c8a0194ad56688827e4b456085a9a4c3
Chandra kishor Gauro <ckgauro@gmail.com>
2020-01-18 19:49:09 -0500

    160

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,11 +12,13 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.title, catData.color))
+            .map(
+              (catData) => CategoryItem(catData.id,  catData.title, catData.color),
+            )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
-          childAspectRatio: 3/2,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
