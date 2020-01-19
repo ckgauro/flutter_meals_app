0ef488cd57718271c4ef6b91cc4bc2a12d3a6ccf
Chandra kishor Gauro <ckgauro@gmail.com>
2020-01-19 10:07:00 -0500

    170

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavorutesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('The Favorites'),
+    );
+  }
+}
